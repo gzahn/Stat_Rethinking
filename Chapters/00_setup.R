972bb2ab08c5664c18787a9a09f85a7d571e0289
gzahn <geoff.gone@gmail.com>
2023-01-09 09:08:13 -0700

    test initial push

---
+++ Chapters/00_setup.R
@@ -0,0 +1,7 @@
+# Rethinking
+
+# SETUP ####
+# install necessary packages
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+install.packages(c("coda","mvtnorm","dagitty")); devtools::install_github("rmcelreath/rethinking")
+
